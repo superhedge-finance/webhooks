39dbd2ddef13e3f60da981372809259795ce9b8e
Paul <phongcpt72@gmail.com>
2024-12-27 00:18:43 +0700

    test

--- src/apis/product/ProductController.ts
+++ src/apis/product/ProductController.ts
@@ -22,11 +22,11 @@ export class ProductController {
   @Inject()
   private readonly contractService: ContractService;
 
-  @Get("")
-  @Returns(200, Array<CreatedProductDto>)
-  async getProducts(@QueryParams("chainId") chainId: number): Promise<Array<CreatedProductDto>> {
-    return await this.productService.getProducts(chainId);
-  }
+  // @Get("")
+  // @Returns(200, Array<CreatedProductDto>)
+  // async getProducts(@QueryParams("chainId") chainId: number): Promise<Array<CreatedProductDto>> {
+  //   return await this.productService.getProducts(chainId);
+  // }
 
   @Get("/get-test-P")
   @Returns(200, AdminWalletDto)
