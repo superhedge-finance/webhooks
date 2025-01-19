400935be71b030884913b34aa8122b31d278bc7b
Paul <phongcpt72@gmail.com>
2025-01-20 00:37:27 +0700

    update webhook

--- src/apis/event/EventController.ts
+++ src/apis/event/EventController.ts
@@ -1,33 +1,44 @@
-import { Controller, Inject } from "@tsed/di";
+import { Controller, Inject, OnInit } from "@tsed/di";
 import { ProductService } from "../product/services/ProductService";
-import { DECIMAL, SUPPORT_CHAINS } from "../../shared/constants";
+import { SUPPORT_CHAINS } from "../../shared/constants";
 import * as cron from "node-cron";
+import { SuperHedgeDataSource } from "../../dal/data-source";
+import { WebhookService } from "./services/WebhookService";
 
 @Controller("/events")
-export class EventsController {
+export class EventsController implements OnInit {
 
   @Inject()
   private readonly productService: ProductService;
 
-  $onInit() {
-    // listen to get transaction from on chain event
-    console.log(SUPPORT_CHAINS);
-    console.log("start cron job")
+  @Inject()
+  private readonly webhookService: WebhookService;
+
+  async $onInit() {
+    // Wait for database connection to be established
+    if (!SuperHedgeDataSource.isInitialized) {
+      await SuperHedgeDataSource.initialize();
+    }
+
+    console.log("start cron job");
     cron.schedule("*/1 * * * *", async () => {
-      for (const chainId of SUPPORT_CHAINS) {
-        await this.productService.removeTransactionOvertime()
-        const products = await this.productService.getProducts(chainId)
-        const productList = products.map(products => products.address);
-        // await this.productService.createWallet()
-        for (const productAddress of productList){
-          const {addressesList,amountsList} = await this.productService.getWithdrawList(productAddress)  
-          if(addressesList && addressesList.length >0)
-          {
-            // update smartcontract
-            const txResult = await this.productService.storeOptionPosition(chainId,productAddress,addressesList,amountsList) 
-            console.log(txResult)
+      try {
+        for (const chainId of SUPPORT_CHAINS) {
+          await this.productService.removeTransactionOvertime();
+          const products = await this.productService.getProducts(chainId);
+          const productList = products.map(products => products.address);
+          
+          for (const productAddress of productList) {
+            const {addressesList, amountsList} = await this.productService.getWithdrawList(productAddress);
+            if (addressesList && addressesList.length > 0) {
+              const txResult = await this.productService.storeOptionPosition(chainId, productAddress, addressesList, amountsList);
+              
+              console.log(txResult);
+            }
           }
         }
+      } catch (error) {
+        console.error("Cron job", error);
       }
     });
   }
