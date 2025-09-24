d2486a0f6c35e267bcd356a7e7c51f06c2c44751
Paul <phongcpt72@gmail.com>
2025-09-24 23:33:11 +0700

    update vercel

---
+++ api/index.ts
@@ -0,0 +1,90 @@
+import { registerProvider } from "@tsed/di";
+import { Logger } from "@tsed/logger";
+import { PlatformExpress } from "@tsed/platform-express";
+import { Server } from "../src/Server";
+import {
+  Product,
+  ProductRepository,
+  SuperHedgeDataSource,
+  User,
+  WithdrawRequest,
+  UserRepository,
+  WithdrawRequestRepository,
+  HistoryRepository,
+  History,
+  RefCodeRepository,
+  RefCode,
+  CouponListRepository,
+  CouponList,
+  CouponAddressListRepository,
+  CouponAddressList,
+} from "../src/dal";
+
+let cachedHandler: any;
+let providersRegistered = false;
+
+function registerProvidersOnce() {
+  if (providersRegistered) return;
+
+  registerProvider({
+    provide: SuperHedgeDataSource,
+    type: "typeorm:datasource",
+    deps: [Logger],
+    async useAsyncFactory(logger: Logger) {
+      await SuperHedgeDataSource.initialize();
+      logger.info("Connected with typeorm to database: PostgreSQL");
+      return SuperHedgeDataSource;
+    },
+    hooks: {
+      $onDestroy(dataSource) {
+        return dataSource.isInitialized && dataSource.destroy();
+      },
+    },
+  });
+
+  registerProvider({
+    provide: UserRepository,
+    useValue: new UserRepository(User, SuperHedgeDataSource.createEntityManager()),
+  });
+
+  registerProvider({
+    provide: ProductRepository,
+    useValue: new ProductRepository(Product, SuperHedgeDataSource.createEntityManager()),
+  });
+
+  registerProvider({
+    provide: WithdrawRequestRepository,
+    useValue: new WithdrawRequestRepository(WithdrawRequest, SuperHedgeDataSource.createEntityManager()),
+  });
+
+  registerProvider({
+    provide: HistoryRepository,
+    useValue: new HistoryRepository(History, SuperHedgeDataSource.createEntityManager()),
+  });
+
+  registerProvider({
+    provide: RefCodeRepository,
+    useValue: new RefCodeRepository(RefCode, SuperHedgeDataSource.createEntityManager()),
+  });
+
+  registerProvider({
+    provide: CouponListRepository,
+    useValue: new CouponListRepository(CouponList, SuperHedgeDataSource.createEntityManager()),
+  });
+
+  registerProvider({
+    provide: CouponAddressListRepository,
+    useValue: new CouponAddressListRepository(CouponAddressList, SuperHedgeDataSource.createEntityManager()),
+  });
+
+  providersRegistered = true;
+}
+
+export default async function handler(req: any, res: any) {
+  if (!cachedHandler) {
+    registerProvidersOnce();
+    const platform = await PlatformExpress.bootstrap(Server);
+    cachedHandler = platform.callback();
+  }
+  return cachedHandler(req, res);
+}
